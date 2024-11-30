87715dfdaaa8fd4b15534910eb17e552c7128f3e
Jean Carlos Nesi <jean.carlos_nesi@hotmail.com>
2024-11-30 13:17:41 -0300

    fix: swagger

--- src/presentation/modules/app.module.ts
+++ src/presentation/modules/app.module.ts
@@ -9,6 +9,8 @@ import { BoardTaskModule } from './board-task.module';
 import { BoardModule } from './board.module';
 import { GlobalModule } from './global.module';
 import { RegisterModule } from './register.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 //#endregion
 
 @Module({
@@ -23,7 +25,10 @@ import { RegisterModule } from './register.module';
         module: AccessModule,
       },
     ]),
-
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'src', 'infrastructure', 'config', 'static'),
+      serveRoot: '/docs', 
+    }),
     RegisterModule,
     RouterModule.register([
       {
